7aaa8e21b1eb63fcba7d2c7afbe5bf4d562696b4
mahima558-devloper <117913824+mahima558-devloper@users.noreply.github.com>
2023-01-04 13:17:38 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,15 +7,25 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  name = 'Angular ' + VERSION.major;
-  title = 'Mahima Form';
-  display = false;
-  userData: any = {};
-  toogle() {
-    this.display = !this.display;
+  // name = 'Angular ' + VERSION.major;
+  // title = 'Mahima Form';
+  // display = false;
+  // userData: any = {};
+  // toogle() {
+  //   this.display = !this.display;
+  // }
+  // getData(data: NgForm) {
+  //   console.warn(data);
+  //   this.userData = data;
+  // }
+  title = 'todo list form';
+  list: any[] = [];
+  newTaskAdd(item: any) {
+    this.list.push({ id: this.list.length, name: item });
+    console.log(this.list);
   }
-  getData(data: NgForm) {
-    console.warn(data);
-    this.userData = data;
+  remove(id: number) {
+    this.list = this.list.filter((item) => item.id !== id);
+    console.warn(id);
   }
 }
